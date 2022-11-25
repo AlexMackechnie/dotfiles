b71ce37e25b7affa2d575499b7de7dd6fa110be2
Alex Mackechnie <mackechniealex@gmail.com>
2022-11-25 17:20:17 +0000

    Harpoon keymaps;

--- nvim/lua/alex/remap.lua
+++ nvim/lua/alex/remap.lua
@@ -14,3 +14,7 @@ nnoremap("<leader>t", "<cmd>NvimTreeToggle<cr>")
 nnoremap("<leader>mm", function() require("harpoon.mark").add_file() end)
 nnoremap("<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end)
 
+nnoremap("<leader>1", function() require("harpoon.ui").nav_file(1) end, silent)
+nnoremap("<leader>2", function() require("harpoon.ui").nav_file(2) end, silent)
+nnoremap("<leader>3", function() require("harpoon.ui").nav_file(3) end, silent)
+nnoremap("<leader>4", function() require("harpoon.ui").nav_file(4) end, silent)
